b61564216f03a8fa0a01a9959fa1af19cac0f089
alevaiano <rm551497@fiap.com.br>
2023-03-28 19:03:01 -0300

    api

---
+++ JsonParser.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JsonParser {
+    
+    //essas duas linhas abaixo extrai a lista de dentro dos [] que está no http
+    //site: regex 101
+    private static final Pattern REGEX_ITEMS = Pattern.compile(".*\\[(.+)\\].*"); //regex para pegar os itens: .*\\[(.+)\\].*
+    private static final Pattern REGEX_ATRIBUTOS_JSON = Pattern.compile("\"(.+?)\":\"(.*?)\"");
+
+    public List<Map<String, String>> parse(String json) {
+        Matcher matcher = REGEX_ITEMS.matcher(json);
+        if (!matcher.find()) {
+
+            throw new IllegalArgumentException("Não encontrou items.");
+        }
+
+        String[] items = matcher.group(1).split("\\},\\{");
+
+        List<Map<String, String>> dados = new ArrayList<>();
+
+        for (String item : items) {
+
+            Map<String, String> atributosItem = new HashMap<>();
+
+            Matcher matcherAtributosJson = REGEX_ATRIBUTOS_JSON.matcher(item);
+            while (matcherAtributosJson.find()) {
+                String atributo = matcherAtributosJson.group(1);
+                String valor = matcherAtributosJson.group(2);
+                atributosItem.put(atributo, valor);
+            }
+
+            dados.add(atributosItem);
+        }
+
+        return dados;
+
+    }
+}
